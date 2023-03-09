0ef1174c0f2c51edd1588aef0dc8db37af53b9fa
Airnov <124378651+Airnov@users.noreply.github.com>
2023-03-09 15:24:38 +0000

    comit

--- index.py
+++ index.py
@@ -9,10 +9,13 @@ def home():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     diameter = float(request.form['diameter'])
-    radius = diameter / 2
-    area = 3.14 * radius ** 2
-    circumference = 2 * 3.14 * radius
-    return render_template('result.html', area=area, circumference=circumference)
+    if diameter <= 0:
+        return render_template('home.html', error="Please enter a positive number")
+    else:
+        radius = diameter / 2
+        area = 3.14 * radius ** 2
+        circumference = 2 * 3.14 * radius
+        return render_template('result.html', area=area, circumference=circumference)
 
 if __name__ == '__main__':
     app.run(debug=True)
