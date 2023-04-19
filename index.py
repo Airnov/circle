660186a3a2afe9c3d1a1ad10f2ad79cf15a751e4
Airnov <124378651+Airnov@users.noreply.github.com>
2023-04-19 14:36:50 +0000

    comit

---
+++ index.py
@@ -0,0 +1,6 @@
+D = float(input("What is the diameter of the circle? "))
+r = D/2   
+A = 3.14*r**2   
+C = 2*3.14*r
+print("The area of the circle is:", A)
+print("the circumfrence of the cricle is:", C)
